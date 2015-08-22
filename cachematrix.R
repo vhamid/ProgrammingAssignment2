e2c275fd89fa05243f414506fb3d1aea0b5c1e8f
Vahid Hamidullah <vhamidullah.github@gmail.com>
2015-08-22 19:08:13 -0400

    Add functionality to skeleton files for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to create a matrix
+## wrapper which can cache its inverse and the cached
+## value when available
 
-## Write a short comment describing this function
-
+## Creates a matrix wrapper which has the ability to
+## cache the inverse of itself
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the inverse of special matrix parameter 'x'
+## created with 'makeCacheMatrix'. Checks if the
+## inverse is cached already before calculating and
+## caching it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
